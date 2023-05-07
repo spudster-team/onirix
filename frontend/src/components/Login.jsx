912fd63f307ecbbf3533eb6e3ecc297839472ba0
Tsierenana Bôtramanagna Gracy <gtsierenana@gmail.com>
2023-05-07 08:13:56 +0300

    add login form

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -2,9 +2,25 @@ import React from "react";
 
 const Login = () => {
     return (
-        <div>
-        <h1>login</h1>
-        </div>
+        <main>
+            <section className="signin-container">
+                <form action="">
+                    <hgroup>
+                        <h2>Se connecter</h2>
+                        <p>Bienvenu sur Onyrix</p>
+                    </hgroup>
+                    <label htmlFor="email" hidden>Adresse email</label>
+                    <input id="email" name="email" type="text" placeholder="E-mail" />
+                    <label htmlFor="email" hidden>Mot de passe</label>
+                    <input id="password" name="password" type="password" placeholder="Mot de passe" />
+                    <button className="button">Se connecter</button>
+                    <p><a href="">Créer un compte</a></p>
+                </form>
+                <div className="form-decoration">
+                    <img src="/assets/logo/logo-full-white.png" alt="" />
+                </div>
+            </section>
+        </main>
     );
 }
 
